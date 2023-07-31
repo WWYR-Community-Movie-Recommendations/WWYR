01582d144a9b9e1e3e603600a6fe7ca3a48422d6
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-07-31 18:45:44 -0500

    Mask ID number and refactor code

--- js/index.js
+++ js/index.js
@@ -3,15 +3,30 @@
 // ***** GLOBALS *****
 let userArray = [];
 
+
 // ***** DOM WINDOWS *****
 // Get user name and user ID from the input fields
 let existingUserForm = document.getElementById('existingUserForm');
 let userIDInput = document.getElementById('userID');
 
+
 // ***** HELPER FUNCTIONS / UTILITIES *****
 
-// Update userArray and store to local storage
+// Check local storage and load user data and userArray
+function loadUserData() {
+
+  // Retrieve existing user data from local storage (if any) and convert into usable JavaScript data, if no data establish empty array
+  let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+  // Restore userArray with data from local storage (if any)
+  userArray = existingUsers;
+
+}
+
+
+// Save userName to local storage in order to retrieve and output userName on other pages
 function persistUserName(existingUserName) {
+
   // ****** Store to Local Storage ******
   // Store userName to local storage, not need to stringify as already type 'string'
   localStorage.setItem('userName', existingUserName);
@@ -20,13 +35,18 @@ function persistUserName(existingUserName) {
 
 // Check if user name and ID exist in userArray
 function checkUserExists(userNameCheck, userIDCheck) {
+
   // Return boolean value (true or false) if matches any userName and ID in userArray
   return userArray.some(function (user) {
     return user.userName === userNameCheck && user.userID === userIDCheck;
   });
+
 }
 
+
 // ***** EVENT HANDLERS *****
+
+// Existing user login, check if credentials match user credentials
 function handleSubmit (event) {
   event.preventDefault();
 
@@ -39,6 +59,7 @@ function handleSubmit (event) {
 
   // Check if userName and UserID match existing data storage, if match, re-direct to home.html and create current user variable to store in local storage
   if (userExists) {
+
     // Store existing user name to local storage
     persistUserName(userNameCheck);
 
@@ -46,36 +67,44 @@ function handleSubmit (event) {
     window.location.href = 'home.html';
 
   } else {
+
     alert('Username and/or UserID do not match. Please check your input.');
+
     // Reset the form
     event.target.reset();
+
   }
+
 }
 
+
+// Ensure numeric value only
 function handleIDInput() {
+
   // Get current value of the ID input
   let inputValue = userIDInput.value;
 
-  // Check if the value is numeric
-  if (isNaN(inputValue)) {
+  //Check if the value is numeric
+  if ( isNaN(inputValue) ) {
+
     // If not numeric don't allow typing
     userIDInput.value = '';
+
+  } else if ( !isNaN(inputValue) ) {
+
+    //Mask user ID number
+    //https://stackoverflow.com/questions/22457344/masking-input-characters-without-type-password
+    userIDInput.type = 'password';
+
   }
+
 }
+
 
 // ***** EXECUTABLE CODE *****
 
-// ***** Check Local Storage and Load into userArray If Any *****
-// Retrieve existing user data from local storage (if any) and convert into usable JavaScript data, if no data establish empty array
-let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+loadUserData();
 
-// Restore userArray with data from local storage (if any)
-userArray = existingUsers;
-
-// ***** User Enters Existing User Info, Check Made if userName and userID Exist, If Exist Direct to Home Page (home.html) *****
-
-// Add listener to limit user ID to four digits only and ensure numeric value only
 userIDInput.addEventListener('input', handleIDInput);
 
-// Add event listener to form 'userForm' where user enters info
 existingUserForm.addEventListener('submit', handleSubmit);
