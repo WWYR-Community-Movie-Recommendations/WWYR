bf54ce44ca7fadfd1bbfe87ed4e188fc70faef4b
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-08-01 08:37:26 -0500

    Display message no movies from community

--- js/communityList.js
+++ js/communityList.js
@@ -57,110 +57,115 @@ function loadMovieArray() {
 
 }
 
-// Render Movie List from computer (only from other users not self)
+// Render Movie List from computer (only from other users not self), no movies from community shared, display message
 function renderMovies() {
 
   movieContainer.innerHTML = ''; // Clear any existing content
 
-  for (let i = 0; i < movieArray.length; i++) {
-    let movieContributor = movieArray[i].userName;
-    let movieTitle = movieArray[i].movieName;
-    let movieComment = movieArray[i].userComment;
-    let movieLink = movieArray[i].videoLink;
-    let movieGenre = movieArray[i].genre;
+  if ( movieArray.length === 0 || movieArray.every(movie => movie.userName === userNameLocalStorage) ) {
+    let message = `${userNameLocalStorage}, no movies have been shared by the community yet!`;
+    displayNoRecommendationsMessage(message);
+  } else {
 
-    if (movieContributor !== userNameLocalStorage) {
+    for (let i = 0; i < movieArray.length; i++) {
+      let movieContributor = movieArray[i].userName;
+      let movieTitle = movieArray[i].movieName;
+      let movieComment = movieArray[i].userComment;
+      let movieLink = movieArray[i].videoLink;
+      let movieGenre = movieArray[i].genre;
 
-      // Create div to house video container and movie details append to movieContainer (master parent)
-      let movieTrailerAndDetailContainer = document.createElement('div');
-      movieTrailerAndDetailContainer.setAttribute('id', 'movie-trailer-detail-container');
-      movieContainer.appendChild(movieTrailerAndDetailContainer);
+      if (movieContributor !== userNameLocalStorage) {
 
-      // Create div with id 'video-container' and append to movieTrailerAndDetailContainer
-      let videoContainer = document.createElement('div');
-      videoContainer.setAttribute('id', 'video-container');
-      movieTrailerAndDetailContainer.appendChild(videoContainer);
+        // Create div to house video container and movie details append to movieContainer (master parent)
+        let movieTrailerAndDetailContainer = document.createElement('div');
+        movieTrailerAndDetailContainer.setAttribute('id', 'movie-trailer-detail-container');
+        movieContainer.appendChild(movieTrailerAndDetailContainer);
 
-      // Create iframe element and append to div videoContainer
-      let videoiFrame = document.createElement('iframe');
-      videoiFrame.setAttribute('id', 'video-iframe');
-      videoiFrame.setAttribute('class', 'youtube-videos');
-      videoiFrame.setAttribute('width', '560');
-      videoiFrame.setAttribute('height', '315');
-      videoiFrame.setAttribute('title', 'YouTube video player');
-      videoiFrame.setAttribute('frameborder', '0');
-      videoiFrame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
-      videoiFrame.setAttribute('allowfullscreen', '');
-      videoContainer.appendChild(videoiFrame);
+        // Create div with id 'video-container' and append to movieTrailerAndDetailContainer
+        let videoContainer = document.createElement('div');
+        videoContainer.setAttribute('id', 'video-container');
+        movieTrailerAndDetailContainer.appendChild(videoContainer);
 
-      // Set the src attribute of the iframe to the embed URL
-      videoiFrame.src = movieLink;
+        // Create iframe element and append to div videoContainer
+        let videoiFrame = document.createElement('iframe');
+        videoiFrame.setAttribute('id', 'video-iframe');
+        videoiFrame.setAttribute('class', 'youtube-videos');
+        videoiFrame.setAttribute('width', '560');
+        videoiFrame.setAttribute('height', '315');
+        videoiFrame.setAttribute('title', 'YouTube video player');
+        videoiFrame.setAttribute('frameborder', '0');
+        videoiFrame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+        videoiFrame.setAttribute('allowfullscreen', '');
+        videoContainer.appendChild(videoiFrame);
 
-      // Create section element and append to div movieContainer (master parent)
-      let filmDetails = document.createElement('section');
-      filmDetails.setAttribute('id', 'film-details');
-      movieTrailerAndDetailContainer.appendChild(filmDetails);
+        // Set the src attribute of the iframe to the embed URL
+        videoiFrame.src = movieLink;
 
-      // Create p element and append to sec filmDetails
-      let movieDetailTitle = document.createElement('p');
-      movieDetailTitle.setAttribute('id', 'title-of-film');
-      movieDetailTitle.setAttribute('class', 'movie-details');
-      movieDetailTitle.textContent = 'Movie Title:  ';
-      filmDetails.appendChild(movieDetailTitle);
+        // Create section element and append to div movieContainer (master parent)
+        let filmDetails = document.createElement('section');
+        filmDetails.setAttribute('id', 'film-details');
+        movieTrailerAndDetailContainer.appendChild(filmDetails);
 
-      // Create <span> element, append to <p> element, output name of movie title
-      let filmTitleText = document.createElement('span');
-      filmTitleText.setAttribute('id', 'title-of-film-text');
-      movieDetailTitle.appendChild(filmTitleText);
-      filmTitleText.textContent = movieTitle;
-      filmTitleText.style.fontWeight = 'normal';
+        // Create p element and append to sec filmDetails
+        let movieDetailTitle = document.createElement('p');
+        movieDetailTitle.setAttribute('id', 'title-of-film');
+        movieDetailTitle.setAttribute('class', 'movie-details');
+        movieDetailTitle.textContent = 'Movie Title:  ';
+        filmDetails.appendChild(movieDetailTitle);
 
-      // Create p element and append to sec filmDetails
-      let movieDetailComment = document.createElement('p');
-      movieDetailComment.setAttribute('id', 'title-of-film');
-      movieDetailComment.setAttribute('class', 'movie-details');
-      movieDetailComment.textContent = 'Movie Comment:  ';
-      filmDetails.appendChild(movieDetailComment);
+        // Create <span> element, append to <p> element, output name of movie title
+        let filmTitleText = document.createElement('span');
+        filmTitleText.setAttribute('id', 'title-of-film-text');
+        movieDetailTitle.appendChild(filmTitleText);
+        filmTitleText.textContent = movieTitle;
+        filmTitleText.style.fontWeight = 'normal';
 
-      // Create <span> element, append to <p> element, output name of movie comment
-      let commentMovieText = document.createElement('span');
-      commentMovieText.setAttribute('id', 'movie-comment-text');
-      movieDetailComment.appendChild(commentMovieText);
-      commentMovieText.textContent = `"${movieComment}"`;
-      commentMovieText.style.fontWeight = 'normal';
-      commentMovieText.style.fontStyle = 'italic';
+        // Create p element and append to sec filmDetails
+        let movieDetailComment = document.createElement('p');
+        movieDetailComment.setAttribute('id', 'title-of-film');
+        movieDetailComment.setAttribute('class', 'movie-details');
+        movieDetailComment.textContent = 'Movie Comment:  ';
+        filmDetails.appendChild(movieDetailComment);
 
-      // Create p element and append to sec filmDetails
-      let genreDetail = document.createElement('p');
-      genreDetail.setAttribute('id', 'genre-of-film');
-      genreDetail.setAttribute('class', 'movie-details');
-      genreDetail.textContent = 'Genre:  ';
-      filmDetails.appendChild(genreDetail);
+        // Create <span> element, append to <p> element, output name of movie comment
+        let commentMovieText = document.createElement('span');
+        commentMovieText.setAttribute('id', 'movie-comment-text');
+        movieDetailComment.appendChild(commentMovieText);
+        commentMovieText.textContent = `"${movieComment}"`;
+        commentMovieText.style.fontWeight = 'normal';
+        commentMovieText.style.fontStyle = 'italic';
 
-      // Create <span> element, append to <p> element, output name of movie comment
-      let genreText = document.createElement('span');
-      genreText.setAttribute('id', 'genre-text');
-      genreDetail.appendChild(genreText);
-      genreText.textContent = movieGenre;
-      genreText.style.fontWeight = 'normal';
+        // Create p element and append to sec filmDetails
+        let genreDetail = document.createElement('p');
+        genreDetail.setAttribute('id', 'genre-of-film');
+        genreDetail.setAttribute('class', 'movie-details');
+        genreDetail.textContent = 'Genre:  ';
+        filmDetails.appendChild(genreDetail);
 
-      // Create p element and append to sec filmDetails
-      let movieDetailContributor = document.createElement('p');
-      movieDetailContributor.setAttribute('id', 'title-of-film');
-      movieDetailContributor.setAttribute('class', 'movie-details');
-      movieDetailContributor.textContent = 'Contributor Name:  ';
-      filmDetails.appendChild(movieDetailContributor);
+        // Create <span> element, append to <p> element, output name of movie comment
+        let genreText = document.createElement('span');
+        genreText.setAttribute('id', 'genre-text');
+        genreDetail.appendChild(genreText);
+        genreText.textContent = movieGenre;
+        genreText.style.fontWeight = 'normal';
 
-      // Create <span> element, append to <p> element, output name of contributor
-      let contributorName = document.createElement('span');
-      contributorName.setAttribute('id', 'contributor-text');
-      movieDetailContributor.appendChild(contributorName);
-      contributorName.textContent = movieContributor;
-      contributorName.style.fontWeight = 'normal';
+        // Create p element and append to sec filmDetails
+        let movieDetailContributor = document.createElement('p');
+        movieDetailContributor.setAttribute('id', 'title-of-film');
+        movieDetailContributor.setAttribute('class', 'movie-details');
+        movieDetailContributor.textContent = 'Contributor Name:  ';
+        filmDetails.appendChild(movieDetailContributor);
+
+        // Create <span> element, append to <p> element, output name of contributor
+        let contributorName = document.createElement('span');
+        contributorName.setAttribute('id', 'contributor-text');
+        movieDetailContributor.appendChild(contributorName);
+        contributorName.textContent = movieContributor;
+        contributorName.style.fontWeight = 'normal';
+      }
+
     }
-
   }
-
 }
 
 // Render Movie List from computer (only from other users not self)
@@ -269,23 +274,41 @@ function renderMoviesOfSelf() {
 
 }
 
+function displayNoRecommendationsMessage(message) {
+  movieContainer.innerHTML = ''; // Clear any existing content
+  let noRecommendationsMessage = document.createElement('p');
+  noRecommendationsMessage.setAttribute('id', 'no-recommendation-message');
+  movieContainer.appendChild(noRecommendationsMessage);
+  noRecommendationsMessage.textContent = message;
+}
+
 
 // ***** EVENT HANDLER(S) *****
 
+// Sort the movieArray based on the selected option, display message if no movies shared by community
 function handleSortButton() {
   let sortOption = sortDropdown.value;
 
-  // Sort the movieArray based on the selected option
   if (sortOption === 'username') {
-
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
-    //localeCompare ensures sorting is done correctly for strings in different languages or locales. Instead of just using movieArray.sort()
-    movieArray.sort((a, b) => a.userName.localeCompare(b.userName));
-    renderMovies();
+    // LocaleCompare ensures sorting is done correctly for strings in different languages or locales. Instead of just using movieArray.sort()
+    // If movie array empty or every movie in array matches existing user name output message, community hasn't shared any movies.
+    if ( movieArray.length === 0 || movieArray.every(movie => movie.userName === userNameLocalStorage) ) {
+      let message = `${userNameLocalStorage}, no movies have been shared by the community yet!`;
+      displayNoRecommendationsMessage(message);
+    } else {
+      movieArray.sort((a, b) => a.userName.localeCompare(b.userName));
+      renderMovies();
+    }
 
   } else if (sortOption === 'movietitle') {
-    movieArray.sort((a, b) => a.movieName.localeCompare(b.movieName));
-    renderMovies();
+    if ( movieArray.length === 0 || movieArray.every(movie => movie.userName === userNameLocalStorage) ) {
+      let message = `${userNameLocalStorage}, no movies have been shared by the community yet!`;
+      displayNoRecommendationsMessage(message);
+    } else {
+      movieArray.sort((a, b) => a.movieName.localeCompare(b.movieName));
+      renderMovies();
+    }
 
   } else if (sortOption === 'self-submitted') {
     if ( movieArray.some(movie => movie.userName === userNameLocalStorage) ) {
@@ -301,8 +324,15 @@ function handleSortButton() {
     }
 
   } else if (sortOption === 'genre') {
-    movieArray.sort((a, b) => a.genre.localeCompare(b.genre));
-    renderMovies();
+
+    // If movie array empty or every movie in array matches existing user name output message, community hasn't shared any movies.
+    if ( movieArray.length === 0 || movieArray.every(movie => movie.userName === userNameLocalStorage) ) {
+      let message = `${userNameLocalStorage}, no movies have been shared by the community yet!`;
+      displayNoRecommendationsMessage(message);
+    } else {
+      movieArray.sort((a, b) => a.genre.localeCompare(b.genre));
+      renderMovies();
+    }
   }
 
 }
